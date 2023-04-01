2cfd95cde5ff742f885d5d740af611683567b1b6
e-a-flow <perezarceedgar@gmail.com>
2023-04-01 11:22:57 -0700

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
+
+class SighnUpView(CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+
+
